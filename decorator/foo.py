a010e60202bf9e5ae99e900d04925c970eaa2cf8
Van Nguyen <van.nguyen@apivio.com>
2017-04-13 16:04:22 -0700

    Initial

--- decorator/foo.py
+++ decorator/foo.py
@@ -1,16 +1,31 @@
 #
+import decorator
 
-def foo(bar):
+def inc(bar):
     return bar + 1
 
 def call_foo_with_arg(functor, arg):
     return functor(arg)
 
+
+@decorator.decorate
+def print_func():
+    print("Printing from parent() function!")
+
+    def first_child():
+        print("Printing from first_child() function!")
+    def second_child():
+        print("Printing from second_child() function!")
+
+    print(first_child());
+    print(second_child());
+
 if __name__ == "__main__":
-    print('foo(2)==3 -> {}'.format(foo(2) == 3))
-    print('foo=', foo)
-    print('foo(2)=', foo(2))
-    print('type(foo)=', type(foo))
+    print('inc(2)==3 -> {}'.format(inc(2) == 3))
+    print('inc=', inc)
+    print('inc(2)=', inc(2))
+    print('type(inc)=', type(inc))
 
-    print('call_foo_with_arg(foo, x)=', call_foo_with_arg(foo, 3))
+    print('call_foo_with_arg(inc, x)=', call_foo_with_arg(inc, 3))
 
+    print_func()
