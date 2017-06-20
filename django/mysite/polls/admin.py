9b317d0c98c4673eb9f02f936dd5e07e7b5609ef
Van Nguyen <van.nguyen@apivio.com>
2017-06-19 17:52:01 -0700

    Cosmetic change

--- django/mysite/polls/admin.py
+++ django/mysite/polls/admin.py
@@ -2,18 +2,33 @@ from django.contrib import admin
 from polls.models import Question, Choice
 
 
+# derived from admin.TabularInline (as opposed to admin.StackedInline) to create a table-like display.
 class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
-    fieldsets = [ (None, {'fields':['question_text']}),
-                  ('Date Information', {'fields':['publication_date'], 'classes':['collapse'] }),
-                ]
+    fieldsets = [
+        # title, map of properties (fields, HTML classes, etc.)
+        ('Q', {'fields':['question_text']}),
+        ('Date Info', {'fields':['publication_date'], 'classes':['collapse'] }),
+    ]
+
+    # Associated/related model to be displayed 'inline'.
     inlines = [ChoiceInline]
+
+    # Columns to display for change-list (enumerate) Question view
     list_display = ('question_text', 'publication_date', 'was_published_recently')
+
+    # side bar list filter. The type of filter possible depends of the type of the field assigned. Must
+    # be derived from models.Field, otherwise exception!
     list_filter = ['publication_date']
-    search_fields = ['question_text']
+
+    # Add search box - list all the Field(s) to do the search!
+    search_fields = ['question_text', 'publication_date']
+
+    # Change list pagination - how many items to display per page!
+    list_per_page = 3
 
 # Register your models here.
 admin.site.register(Question, QuestionAdmin)
