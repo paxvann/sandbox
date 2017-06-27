fe12655980915b05af8cbe90a4d9ae839d470e72
Van Nguyen <van.nguyen@apivio.com>
2017-06-26 18:06:35 -0700

    Added client View

--- django/mysite/polls/views.py
+++ django/mysite/polls/views.py
@@ -1,3 +1,37 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+from polls.models import Question
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-publication_date')[:5]
+    output = '| '.join([p.question_text for p in latest_question_list])
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    """
+    :param request: HttpRequest
+    :param question_id: Integer
+    :return: 
+    """
+    return HttpResponse("You're looking at question, id=%s." % question_id)
+
+
+def results(request, question_id):
+    """
+    :param request: ditto
+    :param question_id: ditto
+    :return:
+    """
+    response = "You're looking at the results of question id=%s"
+    return HttpResponse(response % question_id)
+
+def vote(request, question_id):
+    """
+
+    :param request:
+    :param question_id:
+    :return:
+    """
+    return HttpResponse("You're voting on question, id=%s" % question_id)
